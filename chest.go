a5e50c1712749e5787bf56071ec28f384b649f8a
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-10 11:50:20 +0800

    openCase

--- chest.go
+++ chest.go
@@ -1,11 +1,50 @@
 package main
 
+import (
+	"fmt"
+	"math/rand"
+)
+
 type Chest struct {
 	items []Item
 }
 
-func (c *Chest) NewChest(i []Item) Chest {
+func NewChest(i []Item) Chest {
 	return Chest{
 		items: i,
 	}
+}
+
+//sort the chest by rarity on create chest
+
+func (c *Chest) OpenChest() {
+	// percentage / sum of items
+	totalPercentage := 0.0
+
+	for _, item := range c.items {
+		// fmt.Println("item: ", item)
+		totalPercentage += float64(item.Rarity)
+	}
+	fmt.Println("totalPercentage: ", totalPercentage)
+
+	randomNum := rand.Float64() * totalPercentage
+
+	cumulativePerc := 0.0
+	// chestStat := (cumulativePerc / totalPercentage) * 100
+
+	for _, stat := range c.items {
+		cumulativePerc += float64(stat.Rarity)
+
+		// fmt.Println("cumulativePerc: ", cumulativePerc)
+		// fmt.Println("chestStat: ", chestStat)
+		if randomNum <= cumulativePerc {
+			fmt.Println("------------------------------------")
+			fmt.Println(stat)
+			// fmt.Println("rando: ", randomNum)
+			// fmt.Println("stats: ", cumulativePerc)
+			break
+
+		}
+	}
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,89 @@
 package main
 
-import "fmt"
+var caseItem = []Item{
+	{
+		Name:   "----------------------------M4A4 | Eye of Horus",
+		Rarity: Red,
+	},
+	{
+		Name:   "FAMAS | Waters of Nephthys",
+		Rarity: Pink,
+	},
+	{
+		Name:   "P250 | Apep's Curse",
+		Rarity: Pink,
+	},
+	{
+		Name:   "Glock-18 | Ramese's Reach",
+		Rarity: Pink,
+	},
+	{
+		Name:   "Nova | Sobek's Bite",
+		Rarity: Pink,
+	},
+	{
+		Name:   "AK-47 | Steel Delta",
+		Rarity: Pink,
+	},
+	{
+		Name:   "AWP | Black Nile",
+		Rarity: Pink,
+	},
+	{
+		Name:   "P90 | ScaraB Rush",
+		Rarity: Pink,
+	},
+	{
+		Name:   "M4A1-S | Mud-Spec",
+		Rarity: Pink,
+	},
+	{
+		Name:   "MAG-7 | Copper Coated",
+		Rarity: Pink,
+	},
+	{
+		Name:   "Tec-9 | Mummy's Rot",
+		Rarity: Purple,
+	},
+	{
+		Name:   "MAC-10 | Echoing Sands",
+		Rarity: Purple,
+	},
+	{
+		Name:   "SSG 08 | Azure Glyph",
+		Rarity: Purple,
+	},
+	{
+		Name:   "USP-S | Desert Tactical",
+		Rarity: Purple,
+	},
+	{
+		Name:   "AUG | Snake Pit",
+		Rarity: Blue,
+	},
+	{
+		Name:   "MP7 | Sunbaked",
+		Rarity: Blue,
+	},
+	{
+		Name:   "XM1014 | Hieroglyph",
+		Rarity: Blue,
+	},
+	{
+		Name:   "M249 | Submerged",
+		Rarity: Blue,
+	},
+	{
+		Name:   "R8 Revolver | Inlay",
+		Rarity: Blue,
+	},
+}
 
 func main() {
-	fmt.Println("initial commit")
+
+	anubisCase := NewChest(caseItem)
+
+	for i := 0; i < 100; i++ {
+		anubisCase.OpenChest()
+	}
 }
